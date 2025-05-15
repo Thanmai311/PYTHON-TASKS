7986aabc8eadf4f0e3bf7540369d2599ed867903
Thanmai311 <avvarithanmai@gmail.com>
2025-05-15 07:56:10 +0530

    Create Compare.py

---
+++ Compare.py
@@ -0,0 +1,13 @@
+# Program to compare two numbers
+
+# Taking two numbers as input
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+
+# Comparing the numbers
+if a > b:
+    print("The first number is greater than the second number.")
+elif a < b:
+    print("The first number is less than the second number.")
+else:
+    print("Both numbers are equal.")
