75df8a88687bf21318744f95a83e432eca1e0a8c
Thanmai311 <avvarithanmai@gmail.com>
2025-05-15 07:58:34 +0530

    Create Concate.py

---
+++ Concate.py
@@ -0,0 +1,11 @@
+# Program to concatenate two strings
+
+# Taking input from user
+str1 = input("Enter the first string: ")
+str2 = input("Enter the second string: ")
+
+# Concatenating the strings
+result = str1 + str2
+
+# Printing the result
+print("Concatenated string:", result)
