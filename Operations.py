0a9e6daad72df846c0b58292ec145257d4f07b26
Thanmai311 <avvarithanmai@gmail.com>
2025-05-15 07:57:50 +0530

    Create Operations.py

---
+++ Operations.py
@@ -0,0 +1,17 @@
+# Program to demonstrate arithmetic and logical operators
+
+# Assigning values
+x = int(input())
+y = int(input())
+
+# Arithmetic operations
+print("Addition:", x + y)
+print("Subtraction:", x - y)
+print("Multiplication:", x * y)
+print("Division:", x / y)
+print("Modulus:", x % y)
+
+# Logical operations
+print("x > 5 and y < 10:", x > 5 and y < 10)
+print("x < 5 or y < 10:", x < 5 or y < 10)
+print("not(x == y):", not(x == y))
